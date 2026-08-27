078337d4ff07b0dc4f7cec2b3671155fd3ad83b1
Your Name <you@example.com>
2026-08-27 19:55:14 -0400

    Reconcile retained bastion power state

--- infra/libvirt/versions.tf
+++ infra/libvirt/versions.tf
@@ -2,9 +2,10 @@ terraform {
   required_version = ">= 1.5.0"
 
   required_providers {
+    # v0.9.8 reconciles running=true drift after retained teardown.
     libvirt = {
       source  = "dmacvicar/libvirt"
-      version = "~> 0.9"
+      version = "~> 0.9.8"
     }
   }
 }
